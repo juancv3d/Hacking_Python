1f17b6349744962be13712b0a9f3c528269bf85e
juancv3d <juancvilla96@gmail.com>
2021-05-10 17:33:14 +0000

    adding tcp server and client

---
+++ tcp_client.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python3
+
+import socket
+
+ports = [21,22,23,3306]
+ip=input("IP Target:")
+
+for port in ports:
+    s=socket.socket()
+    print("this is the banner for this port")
+    print(port)
+    
+
+    s.connect((ip,port))
+    answer = s.recv(1024)
+    print(answer)
+    s.close
+
